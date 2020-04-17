efb12df022eabb9a99f0fd41c8c30d23771682c8
herteljt <herteljt@gmail.com>
2020-04-16 23:47:38 -0500

    Added animation. Started with obstacles.

--- main.lua
+++ main.lua
@@ -1,44 +1,66 @@
 function love.load()
-    shipX = 800 / 2
-    shipY = 600 / 2
-    shipAngle = 0
+-- Play Field
+    playAreaWidth = 800
+    playAreaHeight = 600
 
-    shootSfx = love.audio.newSource("sound/laser6.ogg", "static")
-    bullets = {}
+-- Sprite
+    spriteX = 800 / 2
+    spriteY = 600 / 2
+
+    animation = newAnimation(love.graphics.newImage("/sprites/oldHero.png"), 16, 18, 1)
+
+-- Obstacles
+obstacle = {
+  {
+            x = 100,
+            y = 100,
+        },
+        {
+            x = playAreaWidth - 100,
+            y = 100,
+        },
+        {
+            x = playAreaWidth / 2,
+            y = playAreaHeight - 100,
+        }
+}
 
 end
 
 
 function love.update(dt)
--- move small circle
-    if love.keyboard.isDown('right') then
-         shipAngle = shipAngle + 10 * dt
-    end
-
-    if love.keyboard.isDown('left') then
-         shipAngle = shipAngle - 10 * dt
-    end
-
-    -- move large circle
-    if love.keyboard.isDown('w') then
-        shipY = shipY - 5
-    end
-
-    if love.keyboard.isDown('s') then
-        shipY = shipY + 5
-    end
+-- Sprite
 
     if love.keyboard.isDown('d') then
-        shipX = shipX + 5
-        --shootSfx:play()
+      animation.currentTime = animation.currentTime + dt
+       if animation.currentTime >= animation.duration then
+           animation.currentTime = animation.currentTime - animation.duration
+       end
+       spriteX = spriteX + 5
     end
 
     if love.keyboard.isDown('a') then
-        shipX = shipX - 5
+      animation.currentTime = animation.currentTime + dt
+       if animation.currentTime >= animation.duration then
+           animation.currentTime = animation.currentTime - animation.duration
+       end
+       spriteX = spriteX - 5
     end
 
-    if love.keyboard.isDown('f') then
-        playSound(shootSfx)
+    if love.keyboard.isDown('w') then
+      animation.currentTime = animation.currentTime + dt
+       if animation.currentTime >= animation.duration then
+           animation.currentTime = animation.currentTime - animation.duration
+       end
+       spriteY = spriteY - 5
+    end
+
+    if love.keyboard.isDown('s') then
+      animation.currentTime = animation.currentTime + dt
+       if animation.currentTime >= animation.duration then
+           animation.currentTime = animation.currentTime - animation.duration
+       end
+       spriteY = spriteY + 5
     end
 
 -- end program
@@ -46,35 +68,51 @@ function love.update(dt)
          love.event.quit()
     end
 
+--[[
 ---starting to add bullets following https://simplegametutorials.github.io/asteroids/
     for bulletIndex, bullet in ipairs(bullets) do
          love.graphics.setColor(0, 1, 0)
          love.graphics.circle('fill', bullet.x, bullet.y, 5)
     end
+    --]]
 end
 
 function love.draw()
-    love.graphics.setColor(1, 1, 0)
-    love.graphics.circle('fill', shipX, shipY, 30)
+-- Animation
+    local spriteNum = math.floor(animation.currentTime / animation.duration * #animation.quads) + 1
+    love.graphics.draw(animation.spriteSheet, animation.quads[spriteNum], spriteX, spriteY, 0, 4)
 
-    love.graphics.setColor(0, 1, 1)
-    local shipCircleDistance = 20
-    love.graphics.circle(
-        'fill',
-        shipX + math.cos(shipAngle) * shipCircleDistance,
-        shipY + math.sin(shipAngle) * shipCircleDistance,
-        5
-    )
 
-    -- Temporary
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.print('shipAngle: '..shipAngle)
+-- Aisles
+love.graphics.rectangle('fill', playAreaWidth/4, playAreaHeight/4, 10, 20)
+
+
+-- Obstacles
+  for y = -1, 1 do
+        for x = -1, 1 do
+            -- etc.
+
+            for obstacleIndex, obstacle in ipairs(obstacle) do
+              --  love.graphics.setColor(1, 1, 0)
+                love.graphics.circle('fill', obstacle.x, obstacle.y, 80)
+            end
+        end
+   end
 end
 
-function playSound(sound)
-  --sound:rewind(sound)
-  pitchMod = 0.5 + love.math.random(0, 10)/25
-  sound:setPitch(pitchMod)
-  sound:stop()
-  sound:play()
+function newAnimation(image, width, height, duration)
+    local animation = {}
+    animation.spriteSheet = image;
+    animation.quads = {};
+
+    for y = 0, image:getHeight() - height, height do
+        for x = 0, image:getWidth() - width, width do
+            table.insert(animation.quads, love.graphics.newQuad(x, y, width, height, image:getDimensions()))
+        end
+    end
+
+    animation.duration = duration or 1
+    animation.currentTime = 0
+
+    return animation
 end
