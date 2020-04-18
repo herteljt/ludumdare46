8d2580a47bf9d323c3de4c70337b1462f87e1f90
herteljt <herteljt@gmail.com>
2020-04-18 10:28:54 -0500

    incorporated changes into level1 file

--- map test/level1.lua
+++ map test/level1.lua
@@ -1,3 +1,4 @@
+--[[
 walls = {
       {
         x = 100,
@@ -26,6 +27,313 @@ walls = {
         green = 1,
         blue = 1
       }
+
+}
+--]]
+
+wallWidth = 24
+
+walls = {
+    {
+      x = 0*wallWidth,
+      y = 0*wallWidth,
+      width = 1*wallWidth,
+      height = 25*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 0*wallWidth,
+      y = 0*wallWidth,
+      width = 33*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 5*wallWidth,
+      y = 3*wallWidth,
+      width = 6*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 14*wallWidth,
+      y = 3*wallWidth,
+      width = 6*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 4*wallWidth,
+      y = 6*wallWidth,
+      width = 1*wallWidth,
+      height = 10*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 8*wallWidth,
+      y = 6*wallWidth,
+      width = 1*wallWidth,
+      height = 10*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 12*wallWidth,
+      y = 6*wallWidth,
+      width = 1*wallWidth,
+      height = 10*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 16*wallWidth,
+      y = 6*wallWidth,
+      width = 1*wallWidth,
+      height = 10*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 20*wallWidth,
+      y = 6*wallWidth,
+      width = 1*wallWidth,
+      height = 10*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 23*wallWidth,
+      y = 1*wallWidth,
+      width = 1*wallWidth,
+      height = 3*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 28*wallWidth,
+      y = 1*wallWidth,
+      width = 4*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 31*wallWidth,
+      y = 1*wallWidth,
+      width = 1*wallWidth,
+      height = 4*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 32*wallWidth,
+      y = 0*wallWidth,
+      width = 1*wallWidth,
+      height = 22*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 1*wallWidth,
+      y = 20*wallWidth,
+      width = 1*wallWidth,
+      height = 5*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 2*wallWidth,
+      y = 20*wallWidth,
+      width = 1*wallWidth,
+      height = 2*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 2*wallWidth,
+      y = 24*wallWidth,
+      width = 1*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 5*wallWidth,
+      y = 24*wallWidth,
+      width = 10*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 19*wallWidth,
+      y = 24*wallWidth,
+      width = 3*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 21*wallWidth,
+      y = 17*wallWidth,
+      width = 2*wallWidth,
+      height = 6*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 23*wallWidth,
+      y = 17*wallWidth,
+      width = 4*wallWidth,
+      height = 2*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 5*wallWidth,
+      y = 20*wallWidth,
+      width = 2*wallWidth,
+      height = 2*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 5*wallWidth,
+      y = 22*wallWidth,
+      width = 1*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 9*wallWidth,
+      y = 20*wallWidth,
+      width = 2*wallWidth,
+      height = 2*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 9*wallWidth,
+      y = 22*wallWidth,
+      width = 1*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 13*wallWidth,
+      y = 20*wallWidth,
+      width = 2*wallWidth,
+      height = 2*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 13*wallWidth,
+      y = 22*wallWidth,
+      width = 1*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 17*wallWidth,
+      y = 20*wallWidth,
+      width = 2*wallWidth,
+      height = 2*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 17*wallWidth,
+      y = 22*wallWidth,
+      width = 1*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 25*wallWidth,
+      y = 12*wallWidth,
+      width = 2*wallWidth,
+      height = 5*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    },
+
+    {
+      x = 28*wallWidth,
+      y = 22*wallWidth,
+      width = 5*wallWidth,
+      height = 1*wallWidth,
+      red = 0,
+      green = 1,
+      blue = 1
+    }
+
 
 }
 
